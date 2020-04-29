b0f78137492aa0179f7c58e5f7987a6bcf0d45ce
Uranzev <uranzev04@gmail.com>
2020-04-30 01:35:03 +0800

    Add files via upload

---
+++ NotePad/NotePad/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.нээхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.хадгалахToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.гарахToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(674, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.нээхToolStripMenuItem,
+            this.хадгалахToolStripMenuItem,
+            this.гарахToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // нээхToolStripMenuItem
+            // 
+            this.нээхToolStripMenuItem.Name = "нээхToolStripMenuItem";
+            this.нээхToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.нээхToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.нээхToolStripMenuItem.Text = "Нээх";
+            this.нээхToolStripMenuItem.Click += new System.EventHandler(this.нээхToolStripMenuItem_Click);
+            // 
+            // хадгалахToolStripMenuItem
+            // 
+            this.хадгалахToolStripMenuItem.Name = "хадгалахToolStripMenuItem";
+            this.хадгалахToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.хадгалахToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.хадгалахToolStripMenuItem.Text = "Хадгалах";
+            this.хадгалахToolStripMenuItem.Click += new System.EventHandler(this.хадгалахToolStripMenuItem_Click);
+            // 
+            // гарахToolStripMenuItem
+            // 
+            this.гарахToolStripMenuItem.Name = "гарахToolStripMenuItem";
+            this.гарахToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.гарахToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.гарахToolStripMenuItem.Text = "Гарах";
+            this.гарахToolStripMenuItem.Click += new System.EventHandler(this.гарахToolStripMenuItem_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(0, 24);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(674, 357);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 381);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem нээхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem хадгалахToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem гарахToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ NotePad/NotePad/Form1.cs
@@ -0,0 +1,146 @@
+﻿using NotePad.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+        String path;
+        String openingText;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openLast()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Documents|*.txt|Text Documents|*.zev";
+            ofd.InitialDirectory = Settings.Default.lastPath;
+            ofd.ValidateNames = true;
+            ofd.Multiselect = false;
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader srm = new StreamReader(ofd.FileName))
+                {
+                    path = ofd.FileName;
+                    Task<String> text = srm.ReadToEndAsync();
+                    textBox.Text = text.Result;
+                    openingText = text.Result;
+                }
+            }
+        }
+        private void нээхToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openLast();
+            
+        }
+
+        private void хадгалахToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfl = new SaveFileDialog();
+            sfl.Filter = "Text Documents|*.txt|Text Documents|*.zev";
+                   
+            DialogResult dialogResult = sfl.ShowDialog();
+            if(dialogResult == DialogResult.OK)
+            {
+                saveFile(sfl.FileName);
+            }
+
+        }
+
+        private void гарахToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(openingText) && openingText != textBox.Text) showWarningDialog();
+            else this.Close();
+        }
+
+        private void saveFile(String fileName)
+        {
+            FileStream fParameter = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            StreamWriter m_WriterParameter = new StreamWriter(fParameter);
+            m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+            m_WriterParameter.Write(textBox.Text);
+            m_WriterParameter.Flush();
+            m_WriterParameter.Close();
+            path = fileName;
+           
+        }
+
+        private void showWarningDialog()
+        {
+            Form dialog = new Form();
+            dialog.Width = 300;
+            dialog.Height = 160;
+            dialog.Text = "Анхааруулга";
+            Label textLabel = new Label() 
+            { 
+                Left = 50, 
+                Top = 20, 
+                Text = " Өөрчлөлтийг хадгалах уу?.",
+                Width = 200,
+                
+            };
+            Button confirmation = new Button() 
+            {
+                Text = "Тийм", 
+                Left = 20,
+                Width = 100, 
+                Top = 70 
+            };
+            confirmation.Click += (sender, e) => {
+                saveFile(path);
+                dialog.Close(); 
+            };
+            Button decline = new Button()
+            {
+                Text = "Үгүй",
+                Width = 100,
+                Left = 140,
+                Top = 70,
+
+            };
+            decline.Click += (sender, e) => { dialog.Close(); };
+            dialog.Controls.Add(confirmation);
+            dialog.Controls.Add(textLabel);
+            dialog.Controls.Add(decline);
+            dialog.ShowDialog();
+        }
+     
+
+     
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+           
+            if (!string.IsNullOrEmpty(openingText) && openingText != textBox.Text) showWarningDialog();
+            else base.OnFormClosing(e);
+
+
+        }
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            Settings.Default.lastPath = path;
+            Settings.Default.Save();
+            base.OnFormClosed(e);
+        }
+
+    }
+
+    
+       
+        
+        
+
+}
